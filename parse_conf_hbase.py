7b0368d65cf030738dd36768dd032bceef619d71
yassine24 <yassine02409@gmail.com>
2018-07-16 16:15:13 +0200

    training set quasi ok je devrais copier coller le code sur le serveur pour récup les résultats

--- parse_conf_hbase.py
+++ parse_conf_hbase.py
@@ -1,6 +1,19 @@
 from lxml import etree
 import random  as r
 import properties as p
+import numpy as np
+import json
+
+multi = {"hbase.hregion.memstore.flush.size":16777216,
+         "io.storefile.bloom.block.size":65536,
+         "hfile.index.block.max.size":65536,
+         "hbase.regionserver.java.heapsize":536870912
+         }
+
+def create_file_features(filename,data):
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+        outfile.write('\n')
 
 def generate_random_parameters(value):
     if type(value[0]) == float:
@@ -24,29 +37,67 @@ def xml_parser(i):
         name = etree.SubElement(prop, "name")
         value = etree.SubElement(prop, "value")
         name.text = key
+        if multi.get(key):
+            tmp = (tmp*multi[key])
         value.text = str(tmp)
         v.append(tmp)
 
     et = etree.ElementTree(conf)
-    et.write(("hbase-site.xml"+str(i)), pretty_print=True)
+    et.write(("./features/hbase-site.xml"+str(i)), pretty_print=True)
     return v
 
 
+def extract_metrics():
+    avglatency = 0
+    latency = []
+    filepath = 'features0.txt'
+    with open(filepath) as fp:
+        line = fp.readline()
+        cnt = 1
+        while line:
+            if cnt == 2:
+                throughput = line.strip()[32:]
+                throughput = float(throughput)
+                # print(throughput)
+                # print("Line {}: {}".format(cnt, line.strip()))
+            if cnt == 13:
+                latency.append(float(line.strip()[28:]))
 
+            if cnt == 522:
+                latency.append(float(line.strip()[30:]))
+                # print("Line {}: {}".format(cnt, line.strip()))
+
+            line = fp.readline()
+            cnt += 1
+    avglatency = np.average(latency)
+    return throughput, avglatency
+
+
+def generate_conf(n):
+    for i in range(0,n):
+        c = xml_parser(i)
+    return c
+
+def create_training_set(c,metrics):
+    data = {}
+    data['metrics'] = []
+    latency = list(c)
+    throughtput = c
+    throughtput.insert(0, metrics[0])
+    data['metrics'].append(throughtput)
+
+    latency.insert(0, metrics[1])
+    data['metrics'].append(latency)
+    create_file_features("training_set.json", data)
 
 
 if __name__ == '__main__':
-    for i in range(0,3):
-        v = xml_parser(i)
-        print(v)
+    generate_conf(200)
+    # metrics = extract_metrics()
+    # create_training_set(c,metrics)
+
+    # dans ATH il disent prendre 95 95 percentile latency pour la latence car
+    # la latence est diff entre les operation... mais que prendre ? jai pris la moyenne des 2
 
 
 
-
-
-
-
-
-#soit utiliser  In [21]: text_type_determine("2016.23")
-#               Out[21]: 'double'
-# OU bien creer encore un autre dict et y mettre les doubles... cela revient au meme...
