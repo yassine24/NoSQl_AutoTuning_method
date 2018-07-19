b16eafaa11ecc69b270005eb6a3a61e6c052f288
yassine24 <yassine02409@gmail.com>
2018-07-19 14:28:57 +0200

    random forest debut

--- essai.py
+++ essai.py
@@ -1,7 +1,42 @@
-import xml.etree.ElementTree as ET
-from lxml import etree
-
+import json
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+import numpy as np
 if __name__ == '__main__':
-    for i in range(1,21):
-        print(i)
-        tree = ET.parse("hbase_param_file/hbase-site"+str(i)+".xml")
+
+    config = []
+    with open('config.txt') as f:
+        line = f.readline()
+        while line:
+            config.append(line.split(','))
+            line = f.readline()
+    print(config)
+
+    speedup = []
+    with open('speedup.txt') as f:
+        line = f.readline()
+        while line:
+            speedup.append(line.rstrip())
+            line = f.readline()
+    print(speedup)
+
+    latencies = []
+    with open('latencies.txt') as f:
+        line = f.readline()
+        while line:
+            latencies.append(line.rstrip())
+            line = f.readline()
+    print(latencies)
+
+
+
+
+    #n_estimators = ntree : we set ntree to 550 and to 400 when we build throughput
+    # models for update-heavy and read-heavy, respectively
+    #Pour l'instant garder 10 puis nous verrons
+
+
+    # X, y = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
+    # print(X)
+    # print('------------------- Y -----------------')
+    # print(y)
